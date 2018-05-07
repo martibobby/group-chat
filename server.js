aa2eb6af80149e644b03da7da80c84370cc6838f
faul7249 <30452510+faul7249@users.noreply.github.com>
2018-05-07 10:15:18 -0500

    Working Servering .... hopefully

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ var io  = require('socket.io').listen(http);
 var channels = {};
 var sockets = {};
 
-var port = 8014;
+var port = 8004;
 //files we want to serv will be in this dir
 var public_dir = path.join(__dirname, 'public');
 var src_dir = path.join(__dirname, 'src');
@@ -37,9 +37,9 @@ http.listen(port, function(){
 
 /********** VIDEO SERVER *************/
 
-http.listen(port, null, function() {
+/*http.listen(port, null, function() {
     console.log("Listening on port " + port);
-});
+});*/
 //main.use(express.bodyParser());
 
 //app.get('/', function(req, res){ res.sendFile(__dirname + '/index.html'); });
